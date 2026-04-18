07c200f6c6316267395723ff19019f4d27e0983c
Iruag62 <jagtap.gauri.26@protonmail.com>
2026-04-18 15:49:37 +0530

    added main method to the DataTypes class

--- src/fundamentalsAndSyntax/DataTypes.java
+++ src/fundamentalsAndSyntax/DataTypes.java
@@ -1,18 +1,20 @@
 package fundamentalsAndSyntax;
 
 public class DataTypes {
-    // primitive
-    int i = 10;
-    float f = 10.1F; //
-    float f2 = (float) 10.1; //type casting
-    double d = 10.1;
-    char c = 'c';
-    boolean b = true;
-    byte by = 0;
-    short s = 4;
-    long l = 4444444;
+    public static void main(String[] args) {
+        // primitive
+        int i = 10;
+        float f = 10.1F; //
+        float f2 = (float) 10.1; //type casting
+        double d = 10.1;
+        char c = 'c';
+        boolean b = true;
+        byte by = 0;
+        short s = 4;
+        long l = 4444444;
 
-    //type casting
+        //type casting
 
-    // non primitive
+        // non primitive
+    }
 }
