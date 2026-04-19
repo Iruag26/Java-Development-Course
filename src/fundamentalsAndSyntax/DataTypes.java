e436b954f2da99bf25a537684f97207167c343fa
ViKaS s <vikashxrmx+github@gmail.com>
2026-04-19 07:42:20 +0000

    Included few points on variables. source: Duke University - Solving problems with software (Variables video)

--- src/fundamentalsAndSyntax/DataTypes.java
+++ src/fundamentalsAndSyntax/DataTypes.java
@@ -2,6 +2,24 @@ package fundamentalsAndSyntax;
 
 public class DataTypes {
     public static void main(String args[]){
+
+        /*
+         * NOTE: 
+         * In programming languages, when initialising a variable without
+         * a value like:
+         *          int x;
+         *          int y;
+         * No default value value is placed in the locations of these 
+         * varialbes.
+         * This causes undefinied behaviour when accessing the value of 
+         * varialbe (in our case 'x' or 'y' before assignment). 
+         *
+         * Java handles this in two ways:
+         *      - assigning a default value to instance variables
+         *      - explicit error is thrown for using varialbe before 
+         *      initial assignement
+         */
+
     // primitive
     int i = 10;
     float f = 10.1F; 
